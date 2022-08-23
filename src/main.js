2e4bfef45a74b99804a60e351eebc3b96233ef52
Pierre Baudin <pvbaudin@ucsc.edu>
2022-08-23 15:37:41 -0700

    about to try the method from the documentation

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
+// import Vue from 'vue'
+// import App from './App.vue'
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
+
+
+import Vue from "vue";
+// import * as Vue from 'vue';
+import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import BootstrapVue from "bootstrap-vue";
+  Vue.config.productionTip = false;
+  Vue.use(BootstrapVue);
+  new Vue({
+    render: (h) => h(App),
+  }).$mount("#app");
