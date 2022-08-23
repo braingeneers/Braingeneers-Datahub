652021199fd44338faaa33a27b40c49a0de39ecd
Pierre Baudin <pvbaudin@ucsc.edu>
2022-08-23 16:15:23 -0700

    dang, actually hadn't even version controlled the file i was branching on, derp

---
+++ src/components/Plates.vue
@@ -0,0 +1,52 @@
+<script>
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      plates: [],
+      error: null
+    }
+  },
+  async mounted () {
+    try {
+      const response = await axios.get('http://localhost:1337/api/plates')
+      this.plates = response.data.data
+    } catch (error) {
+      this.error = error;
+    }
+  }
+}
+</script>
+
+
+<template>
+  <b-container>
+    <div v-if="plates.length">
+      <b-row>
+        <div v-bind:key="plate.id" v-for="plate in plates">
+          <b-col l="4">
+            <b-card
+              v-bind:img-src="`https://via.placeholder.com/150`"
+              v-bind:title="plate.attributes.name"
+              img-alt="Image"
+              img-top
+              tag="article"
+              style="max-width: 20rem;"
+              class="mb-2"
+            >
+              <b-card-text>{{ `${plate.attributes.description}` }}</b-card-text>
+              <!-- <span>
+                <strong>Price: ${{ `${plate.price}` }} </strong>
+              </span> -->
+              <b-button @click="placeOrder" variant="primary">Order plate</b-button>
+            </b-card>
+          </b-col>
+        </div>
+      </b-row>
+    </div>
+    <div v-else>
+      <h5>Fetching plates . . .</h5>
+    </div>
+  </b-container>
+</template>
