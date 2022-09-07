c1cf9174db25726101102178615cdcf09a608d7e
Pierre Baudin <pvbaudin@ucsc.edu>
2022-09-07 16:20:18 -0700

    broke it

--- src/components/Wells.vue
+++ src/components/Wells.vue
@@ -50,6 +50,31 @@ export default {
         </b-row>
         <div v-if="wells.length">
             <b-row>
+                <table id="picture-grid">
+                    <tr v-for="row in 4" :key="row">
+                    <td v-for="col in 6" :key="row * 10 + col">
+                        <b-col l="1">
+                            <b-card 
+                                v-bind:img-src="`https://via.placeholder.com/200`" 
+                                v-bind:title="wells[0].attributes.name"
+                                img-alt="Image" 
+                                img-top 
+                                tag="article" 
+                                style="max-width: 20rem;" 
+                                class="mb-2">
+                                <b-card-text>fart</b-card-text>
+                                <b-card-text>{{ `${well.attributes.description}` }}</b-card-text>
+                                <!-- create unique collapse toggle for each card in for loop -->
+                                <!-- <b-button v-b-toggle="'collapse-' + well.id" variant="primary">Toggle</b-button>
+                                <b-collapse v-bind:id="'collapse-' + well.id" class="mt-2">
+                                    <b-card>Hello!</b-card>
+                                </b-collapse>  -->
+                            </b-card>
+                        </b-col>
+                    </td>
+                    </tr>
+                </table>
+                
                 <div v-bind:key="well.id" v-for="well in wells">
                     <b-col l="1">
                         <b-card 
@@ -66,25 +91,6 @@ export default {
                             <b-collapse v-bind:id="'collapse-' + well.id" class="mt-2">
                                 <b-card>Hello!</b-card>
                             </b-collapse> 
-                                <!-- <b-card-text>
-                                    <b-row>
-                                        <b-col>
-                                            <b-card-text>Plate: {{ well.attributes.plate.data.attributes.name }}</b-card-text>
-                                            <b-card-text>Experiment: {{ well.attributes.plate.data.attributes.experiment.data.attributes.name }}</b-card-text>
-                                            <b-card-text>Sample: {{ well.attributes.sample.data.attributes.name }}</b-card-text>
-                                            <b-card-text>Sample Type: {{ well.attributes.sample.data.attributes.sample_type.data.attributes.name }}</b-card-text>
-                                            <b-card-text>Sample Description: {{ well.attributes.sample.data.attributes.description }}</b-card-text>
-                                        </b-col>
-                                        <b-col>
-                                            <b-card-text>Sample Concentration: {{ well.attributes.sample.data.attributes.concentration }}</b-card-text>
-                                            <b-card-text>Sample Volume: {{ well.attributes.sample.data.attributes.volume }}</b-card-text>
-                                            <b-card-text>Sample Units: {{ well.attributes.sample.data.attributes.units }}</b-card-text>
-                                            <b-card-text>Sample Date: {{ well.attributes.sample.data.attributes.date }}</b-card-text>
-                                            <b-card-text>Sample Time: {{ well.attributes.sample.data.attributes.time }}</b-card-text>
-                                        </b-col>
-                                    </b-row>
-                                </b-card-text>
-                            </b-collapse> -->
                         </b-card>
                     </b-col>
                 </div>
