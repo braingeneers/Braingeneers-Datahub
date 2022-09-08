5bcd666f29d0adc8ae2a99e078c5b6f327492e63
Pierre Baudin <pvbaudin@ucsc.edu>
2022-09-08 15:28:11 -0700

    trying to fix spacing of date item

--- src/components/Experiments.vue
+++ src/components/Experiments.vue
@@ -29,6 +29,7 @@ export default {
 <style>
     .text-properties{
         display: inline;
+        min-width: 2000px;
     }
 </style>
 
@@ -43,16 +44,20 @@ export default {
                 <b-card>
                 
                 <div class="text-left">
+                    <b-col md="6" class="text-properties">
+                <div class="text-properties">
                     <h4 class="text-properties">{{experiment.attributes.name}}  </h4>
+                </div>
+                </b-col>
                     <!-- horizontal white space -->
 
-                    <b-card-text class="text-properties float-center">Date</b-card-text>
+                    <b-card-text class="text-properties float-center">Date {{experiment.attributes.updatedAt}}</b-card-text>
                     <b-button class="float-right" v-b-toggle="'collapse-' + experiment.id" variant="primary">...</b-button>
                 </div>
                 <b-collapse v-bind:id="'collapse-' + experiment.id" class="mt-2">
                     <b-card>Hello!
-                        <b-card-text class="text-properties">{{` ${experiment.attributes.plates.data[0].attributes.name}` }} </b-card-text>
-                        <b-card-text class="text-properties text-center"> biiiiiiif </b-card-text>
+                        <!-- <b-card-text class="text-properties">{{` ${experiment.attributes.plates.data[0].attributes.name}` }} </b-card-text> -->
+                        <b-card-text class="text-properties text-center">placehjolder </b-card-text>
                         <div v-bind:key="plate.id" v-for="plate in experiment.attributes.plates.data">
                             <b-button v-on:click="redirectToURL_wells(plate.attributes.name)">{{plate.attributes.name}}</b-button>
                         </div>
