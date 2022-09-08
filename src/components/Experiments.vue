8dd86280ac0c3a5c2b63138b5508ade242a9cb9e
Pierre Baudin <pvbaudin@ucsc.edu>
2022-09-08 16:01:32 -0700

    added subcards in experiment collapse

--- src/components/Experiments.vue
+++ src/components/Experiments.vue
@@ -52,6 +52,38 @@ export default {
                     <b-button class="float-right" v-b-toggle="'collapse-' + experiment.id" variant="primary">...</b-button>
                 </div>
                 <b-collapse v-bind:id="'collapse-' + experiment.id" class="mt-2">
+                    <div>
+  <b-card-group>
+    <b-card title="Title" img-src="https://placekitten.com/g/300/450" img-alt="Image" img-top>
+      <b-card-text>
+        This is a wider card with supporting text below as a natural lead-in to additional content.
+        This content is a little bit longer.
+      </b-card-text>
+      <template #footer>
+        <small class="text-muted">Last updated 3 mins ago</small>
+      </template>
+    </b-card>
+
+    <b-card title="Title" img-src="https://placekitten.com/g/300/450" img-alt="Image" img-top>
+      <b-card-text>
+        This card has supporting text below as a natural lead-in to additional content.
+      </b-card-text>
+      <template #footer>
+        <small class="text-muted">Last updated 3 mins ago</small>
+      </template>
+    </b-card>
+
+    <b-card title="Title" img-src="https://placekitten.com/g/300/450" img-alt="Image" img-top>
+      <b-card-text>
+        This is a wider card with supporting text below as a natural lead-in to additional content.
+        This card has even longer content than the first to show that equal height action.
+      </b-card-text>
+      <template #footer>
+        <small class="text-muted">Last updated 3 mins ago</small>
+      </template>
+    </b-card>
+  </b-card-group>
+</div>
                     <b-card>Hello!
                         <!-- <b-card-text class="text-properties">{{` ${experiment.attributes.plates.data[0].attributes.name}` }} </b-card-text> -->
                         <b-card-text class="text-properties text-center">placehjolder </b-card-text>
