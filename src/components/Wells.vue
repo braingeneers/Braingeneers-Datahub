9f7100c51fc63314a415c68455dd86b58dc7a953
Pierre Baudin <pvbaudin@ucsc.edu>
2022-09-14 17:18:39 -0700

    added loading bar for preloading images

--- src/components/Wells.vue
+++ src/components/Wells.vue
@@ -18,6 +18,8 @@ export default {
             api_token: "63e7b69b04c8f9d9c1c368a10ee586de676e5ae58b970225b4eb4d4ac8a633f141e98c656f296fabb0c1be54b01b23c152d09f41a562f7d6acde3865c3b53e33af5e1fbb4680adf71552f6b8e733e5df460a1c8fa32f42e2ccc26f204198bddc9382dcfe5d1af774465ce706baa6fcda5e0c0b8050f90a74f86dd8cffa1ccecb",
             api_endpoint: "http://localhost:1337",
             //image viewer stuff
+            progress: 0,
+            images: [],
             uuid:"2022-07-11-i-connectoid-3",
             groupID:"C", 
             endpoint: "https://s3-west.nrp-nautilus.io/braingeneers/imaging",
@@ -247,6 +249,17 @@ export default {
                     console.log("At maximum focal length")
                 }
             },
+            loadAllImagesFromLayer(row, col, layer){
+                console.log("load all images from layer", row, col, layer)
+                // fetch image from url with axios
+                // axios.get(`http://localhost:1337/api/wells${this.filter_params}&populate=*`)
+                var img = new Image()
+                // img.src = `${this.endpoint}/${this.uuid}/images/${this.manifest.captures[this.curTimestampIndex].timestamp}/${this.curZ}.png`
+                img.src = "https://placekitten.com/g/600/600"
+                this.images.push(img)
+
+            },
+        
 
         }
 }
@@ -311,6 +324,7 @@ export default {
                                                     <img class="img-responsive" @error="missing($event)" :src="`${endpoint}/${uuid}/images/${manifest.captures[curTimestampIndex]}/camera${groupID}${row}${col}/${curZ + 1}.jpg`" style="max-height:300px; display:block;">
                                                     Current Timestamp: {{curTimestampIndex+1}}/{{manifest.captures.length}}
                                                     T: {{ manifest.captures[curTimestampIndex] }} Z: {{ curZ+1 }}/{{manifest.stack_size}}
+                                                    <vue-ellipse-progress :size="25" :progress="progress"/>
                                                     <div style="padding-top: 1vw">
                                                         <b-button id="Arrow48Left" type="button" v-on:click="OnArrow48LeftClick">
                                                                 Back 48 Timesteps
@@ -334,7 +348,6 @@ export default {
                                                             Next Focal View
                                                         </b-button>
                                                     </div>
-                                                    <!-- <p class="my-4">cute cat</p> -->
                                                     samples
                                                     <b-button pill style="display:block;" v-b-toggle="'collapse-samples-' + wells[((row-1)*6+col)-1].id" variant="primary"> List Samples</b-button>
                                                     <b-collapse v-bind:id="'collapse-samples-' + wells[((row-1)*6+col)-1].id" class="mt-2">
@@ -387,26 +400,11 @@ export default {
 
                                                 </b-modal>
                                         </div>                               
-                                        <!-- <b-button id="my-button" v-b-toggle="'collapse-' + wells[((row-1)*6+col)-1].id" variant="primary" ></b-button>
-                                        <b-collapse v-bind:id="'collapse-' + wells[((row-1)*6+col)-1].id" class="mt-2">
-                                            <h>Hello!</h>
-                                        </b-collapse> -->
-                                        <!-- elements removed from card
-                                            @click="showModal('modal-centere' + wells[((row-1)*6+col)-1].id)"
-                                            @click="showModal('modal-centere' + wells[((row-1)*6+col)-1].id)"
-                                            v-bind:title="wells[((row-1)*6+col)-1].attributes.name"
-                                        v-bind:img-src="`https://placekitten.com/g/200/200`"
-                                        img-alt="Image" 
-                                        img-top 
-                                        -->
                                     </b-card>
                                 </b-col>
                             </div>
                         </b-row>
                     </div>
-                <!-- </div> -->
-                <!-- </table> -->
-            <!-- </b-row> -->
         </div>
         <div v-else>
             <h5>No wells found . . .</h5>
--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import App from "./App.vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import router from './router'
+import VueEllipseProgress from 'vue-ellipse-progress';
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
+Vue.use(VueEllipseProgress);
 new Vue({
   router,
   render: (h) => h(App)
