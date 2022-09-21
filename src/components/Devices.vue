fea00f68516dad583888f7d5eba9f5a1ecec9ca3
Pierre Baudin <pvbaudin@ucsc.edu>
2022-09-21 14:59:09 -0700

    devices get organized, about to make api call filter results instead

--- src/components/Devices.vue
+++ src/components/Devices.vue
@@ -42,76 +42,61 @@
     <template>
       <b-container>
         <div v-if="interaction_things.length">
-          <!-- <b-row> -->
             <div v-bind:key="device.id" v-for="device in interaction_things">
-                <!-- <b-row> -->
-
+                <div v-if="type === device.attributes.type">
                     <b-card>
-                    <div v-if="type === device.attributes.type">
-                        <h4 class="text-properties"> yup  </h4>
-                    </div>
-                    <div class="text-left">
-                        <h4 class="text-properties">{{device.attributes.name}}  </h4>
-    
-                        <!-- horizontal white space -->
-    
-                        <b-card-text class="text-properties text-center"> {{device.attributes.updatedAt | luxon}}</b-card-text>
-                        <b-button class="float-right" v-b-toggle="'collapse-' + device.id" variant="primary">...</b-button>
-                    </div>
-                    <b-collapse v-bind:id="'collapse-' + device.id" class="mt-2">
-                        <div>
-                            <b-card-group>
-                                <b-card title="Type">
-                                <b-card-text class="text-left">
-                                    {{device.attributes.type}}
-                                </b-card-text>
-                                <!-- <div v-bind:key="interaction_thing.id" v-for="interaction_thing in device.attributes.interaction_things.data">
-                                        <b-button v-on:click="redirectToURL_wells(interaction_thing.attributes.name)">{{interaction_thing.attributes.name}}</b-button>
-                                </div> -->
-                                <template #footer>
-                                    <small class="text-muted">footer</small>
-                                </template>
-                                </b-card>
-    
-                                <b-card title="device-Notes">
-                                <b-card-text class="text-left">
-                                    {{device.attributes.description}}
-                                </b-card-text>
-                                <template #footer>
-                                    <small class="text-muted">Last updated 3 mins ago</small>
-                                </template>
-                                </b-card>
-    
-                                <b-card title="Plates">
-                                <b-card-text>
-                                    This is where we list plates
-                                </b-card-text>
-                                    <!-- <div v-bind:key="plate.id" v-for="plate in device.attributes.plates.data"> -->
-                                    <div v-if="device.attributes.plate.data">
-                                        <b-button v-on:click="redirectToURL_wells(device.attributes.plate.data.attributes.name)">{{device.attributes.plate.data.attributes.name}}</b-button>
-                                    </div>
-                                <template #footer>
-                                    <small class="text-muted">Last updated 3 mins ago</small>
-                                </template>
-                                </b-card>
-                            </b-card-group>
+
+                        <div class="text-left">
+                            <h4 class="text-properties">{{device.attributes.name}}  </h4>
+        
+                            <!-- horizontal white space -->
+        
+                            <b-card-text class="text-properties text-center"> Last updated: {{device.attributes.updatedAt | luxon}}</b-card-text>
+                            <b-button class="float-right" v-b-toggle="'collapse-' + device.id" variant="primary">...</b-button>
                         </div>
-                    </b-collapse> 
-                    <!-- <span> -->
-                        <!-- <div v-bind:key="well.id" v-for="well in device.attributes.wells.data">
-                        <strong>{{ `${well.attributes.name}` }}</strong>
-                        </div> -->
-                    <!-- </span> -->
-                    <!-- <button v-on:click="redirectToURL(device.attributes.name)"> Plates </button> -->
-                    <!-- <router-link to="/wells/" + ${device.attributes.name} >Wells</router-link>  -->
-    
-                    <!-- removed bits
-                    v-bind:title="device.attributes.name"
-                 -->
+                        <b-collapse v-bind:id="'collapse-' + device.id" class="mt-2">
+                            <div>
+                                <b-card-group>
+                                    <b-card title="Type">
+                                        <b-card-text class="text-left">
+                                            {{device.attributes.type}}
+                                        </b-card-text>
+                                        <!-- <div v-bind:key="interaction_thing.id" v-for="interaction_thing in device.attributes.interaction_things.data">
+                                                <b-button v-on:click="redirectToURL_wells(interaction_thing.attributes.name)">{{interaction_thing.attributes.name}}</b-button>
+                                        </div> -->
+                                        <template #footer>
+                                            <small class="text-muted">footer</small>
+                                        </template>
+                                    </b-card>
+        
+                                    <b-card title="device-Notes">
+                                        <b-card-text class="text-left">
+                                            {{device.attributes.description}}
+                                        </b-card-text>
+                                        <template #footer>
+                                            <small class="text-muted">Last updated 3 mins ago</small>
+                                        </template>
+                                    </b-card>
+        
+                                    <b-card title="Plates">
+                                        <b-card-text>
+                                            This is where we list plates
+                                        </b-card-text>
+                                            <!-- <div v-bind:key="plate.id" v-for="plate in device.attributes.plates.data"> -->
+                                            <div v-if="device.attributes.plate.data">
+                                                <b-button v-on:click="redirectToURL_wells(device.attributes.plate.data.attributes.name)">{{device.attributes.plate.data.attributes.name}}</b-button>
+                                            </div>
+                                        <template #footer>
+                                            <small class="text-muted">Last updated 3 mins ago</small>
+                                        </template>
+                                    </b-card>
+                                </b-card-group>
+                            </div>
+                        </b-collapse> 
                     </b-card>
-                <!-- </b-row> -->
+                </div>
+
             </div>
-          <!-- </b-row> -->
         </div>
         <div v-else>
           <h5>Fetching devices . . .</h5>
