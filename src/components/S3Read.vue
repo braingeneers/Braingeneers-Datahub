25efacd2a149df67f0417ebdddb5851d71d28ff6
Pierre Baudin <pvbaudin@ucsc.edu>
2022-10-06 10:52:28 -0700

    url created correctly, CORS error blocks resource

--- src/components/S3Read.vue
+++ src/components/S3Read.vue
@@ -30,18 +30,18 @@ export default {
             accessKeyId: `${process.env.VUE_APP_AWS_ACCESS_KEY_ID}`,
             secretAccessKey: `${process.env.VUE_APP_AWS_SECRET_ACCESS_KEY}`,
         })
-        var ep = new AWS.Endpoint('awsproxy.example.com');
+        var ep = new AWS.Endpoint('https://nrp-nautilus.io');
         const s3 = new AWS.S3({endpoint: ep});
         console.log(s3.endpoint);
         // console.log("hi");
-        console.log(s3);
+        // console.log(s3);
         const params = {
-            Bucket: 'strapi-shadows-auth0',
-            Key: 'test.txt'
+            Bucket: 's3-west',
+            Key: 'braingeneers/imaging/2022-03-23-i-UCB-bio/images/2022-03-23T21:55:43/cameraG21/1.jpg'
         };
         console.log("hi");
         await s3.getObject(params, function (err, data) {
-            if (err) console.log(err, err.stack); // an error occurred
+            if (err) console.log(err); // an error occurred
             else console.log(data);           // successful response
         });
     },
