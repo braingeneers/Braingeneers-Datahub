d05f72431c830fc142590b575054de70868f20cf
Pierre Baudin <pvbaudin@ucsc.edu>
2022-10-12 15:06:39 -0700

    debugged load_data

--- src/components/S3Read.vue
+++ src/components/S3Read.vue
@@ -32,20 +32,20 @@ export default {
     },
     data() {
         return {
-            metadata : {},
-            trace1 : {
-                    x: [1, 2, 3, 4],
-                    y: [10, 15, 13, 17],
-                    mode: 'markers',
-                    type: 'scatter'
-                },
-            trace2 : {
-                    x: [2, 3, 4, 5],
-                    y: [16, 5, 11, 9],
-                    mode: 'lines',
-                    type: 'scatter'
-                },
-            data : [],
+            metadata: {},
+            trace1: {
+                x: [1, 2, 3, 4],
+                y: [10, 15, 13, 17],
+                mode: 'markers',
+                type: 'scatter'
+            },
+            trace2: {
+                x: [2, 3, 4, 5],
+                y: [16, 5, 11, 9],
+                mode: 'lines',
+                type: 'scatter'
+            },
+            data: [],
             layout: {
                 title: "My graph"
             },
@@ -76,31 +76,74 @@ export default {
         //     console.log(data);
         //     this.metadata = data;
         // });
-        console.log(JSON.stringify(this.metadata, 0, 2));
+        // console.log(JSON.stringify(this.metadata, 0, 2));
+        this.load_data(this.metadata, "experiment4", 0, 60000);
         // JSON.parse(this.metadata.body.data)
     },
     methods: {
-        async load_metadata(uuid){
+        async load_metadata(uuid) {
             const params = {
                 Bucket: 'braingeneers',
                 Key: `ephys/${uuid}/metadata.json`
             };
             const response = await this.s3.getObject(params).promise();
-            
-            // , function (err, data) {
-            //     if (err){
-            //         console.error(err); // an error occurred
-            //         return null
-            //     }
-            //     else {
-            //         console.log(JSON.stringify(data, 0, 2));  
-            //         return data
-            //         // console.log(JSON.stringify(this.metadata, 0, 2));
-            //         }         // successful response
-            // });
-            // console.log("inner response: ", response);
             return response
         },
+        async load_data(metadata, experiment, offset = 0, data_length = null, channels = null, parallelism = 'auto') {
+            var dataset_size = 0
+            // console.log(channels, hardware, batch_uuid, experiment_name, offset, data_length, parallelism)
+            console.log("experiment: ", experiment);
+            // console.log("metadata: ", metadata);
+            console.log("offset: ", offset);
+            console.log("data_length: ", data_length);
+            console.log("channels: ", channels);
+            console.log("parallelism: ", parallelism);
+            // console.log("metadata[ephys_experiments]: ", JSON.parse(metadata['ephys_experiments']));
+            var blocks = "nf" // not found
+            var experiment_data
+            for (var i in metadata['ephys_experiments']) {
+                console.log(metadata['ephys_experiments'][i].name)
+                if (experiment === metadata['ephys_experiments'][i].name) {
+                    experiment_data = metadata['ephys_experiments'][i]
+                    blocks = experiment_data.blocks
+                    break
+                }
+            }
+            console.log("blocks: ", blocks);
+            if (blocks === "nf") {
+                console.error("experiment not found")
+                return
+            }
+            // const blocks = metadata['ephys_experiments'][]['blocks']
+            for (var block in blocks){
+                console.log("num frames: ", blocks[block].num_frames);
+                dataset_size += blocks[block].num_frames
+            }
+            if (offset + data_length > dataset_size) {
+                const error = `offset + length (${offset + data_length}) exceeds dataset size (${dataset_size})`
+                console.error(error)
+            }
+            // console log datatype of experiment object
+
+
+
+            //python line from braingeneerspy to account for more complicated structures: experiment_name = list(metadata['ephys_experiments'].keys())[experiment] if isinstance(experiment, int) else experiment
+            // if (typeof experiment === 'number'){
+            //     console.log("boof")
+
+            // }
+            const experiment_name = experiment
+
+            const batch_uuid = metadata['uuid']
+            const hardware = experiment_data.hardware
+
+            console.log(channels, hardware, batch_uuid, experiment_name, offset, data_length, parallelism)
+            // const data = await this.load_data_maxwell(metadata, batch_uuid, experiment_name, channels, offset, length)
+
+            // return data
+
+        },
+
         async getImage(s3) {
             const data = s3.getObject(
                 {
